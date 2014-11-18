b5d9a5f535e85ebab0309c2fe88ecfc16c866d84
Detrav <witalyezep@gmail.com>
2014-11-18 17:37:51 +0300

    Синглтон для загрузки настроек из базы

---
+++ AniLiteWebSite/Core/SConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AniLiteWebSite.Core.DataBase.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AniLiteWebSite.Core
+{
+    public class SConfiguration
+    {
+        public UserRole DefaultRole;
+    }
+}
