73575ee7b5f74284ee63b48d29f960c859e67add
Detrav <witalyezep@gmail.com>
2014-11-20 21:07:04 +0300

    Сделать ЧекБокс

--- AniLiteWebSite/Core/HtmlHelper/BootstrapHtmlHelper.cs
+++ AniLiteWebSite/Core/HtmlHelper/BootstrapHtmlHelper.cs
@@ -75,8 +75,11 @@ namespace AniLiteWebSite.Core.HtmlHelper
             sb.AppendLine(LabelExtensions
                 .LabelFor(html, expression, new { @class = String.Format("col-sm-{0} control-label", 12 - col_sm) }).ToHtmlString());
             sb.AppendLine(String.Format("<div class=\"col-sm-{0}\">", col_sm));
+            string value;
+            try { value = expression.Compile()(html.ViewData.Model).ToString(); }
+            catch { value = ""; }
             sb.AppendLine(LabelExtensions
-                .Label(html, expression.Compile()(html.ViewData.Model).ToString(), new { @class = "form-control"}).ToHtmlString());
+                .Label(html, value, new { @class = "form-control"}).ToHtmlString());
             sb.AppendLine("</div>");
             sb.AppendLine("</div>");
             return new MvcHtmlString(sb.ToString());
@@ -116,6 +119,26 @@ namespace AniLiteWebSite.Core.HtmlHelper
             
             sb.AppendLine(String.Format(
                 "<input type=\"button\" value=\"Добавить eщё\" class=\"form-control btn btn-xs btn-success\">", name));
+            sb.AppendLine("</div>");
+            sb.AppendLine("</div>");
+            return new MvcHtmlString(sb.ToString());
+        }
+
+        public static MvcHtmlString BSFGForCheckBox<TModel>(
+           this HtmlHelper<TModel> html,
+           Expression<Func<TModel, bool>> expression,
+           int col_sm,
+           string PlaceHolder)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<div class=\"form-group\">");
+            sb.AppendLine(LabelExtensions
+                .LabelFor(html, expression, new { @class = String.Format("col-sm-{0} control-label", 12 - col_sm) }).ToHtmlString());
+            sb.AppendLine(String.Format("<div class=\"col-sm-{0}\">", col_sm));
+            sb.AppendLine(InputExtensions
+                .CheckBoxFor(html, expression, new { @class = "form-control"}).ToHtmlString());
+            sb.AppendLine(ValidationExtensions
+                .ValidationMessageFor(html, expression).ToHtmlString());
             sb.AppendLine("</div>");
             sb.AppendLine("</div>");
             return new MvcHtmlString(sb.ToString());
